9dcf64735652335aa01a2f5bfb09c58b5c1e8f70
grety <snl1016@163.com>
2022-07-28 00:00:52 +0800

    enter

--- lifecycle/src/lib.rs
+++ lifecycle/src/lib.rs
@@ -9,12 +9,25 @@ fn longest<'a>(x:&'a str,y:&'a str) -> &'a str{
     }
 }
 
-pub fn lifecycle(){
-   let s1 = String::from("abcde");
-   let s2 = String::from("dfdf");
 
-   let r=longest(&s1,&s2);
-   println!("the longest is :{}",r);
+struct Student<'a>{
+    name:&'a str,
+    sex:&'a str,
+}
+
+pub fn lifecycle(){
+//    let s1 = String::from("abcde");
+//    let s2 = String::from("dfdf");
+
+//    let r=longest(&s1,&s2);
+//    println!("the longest is :{}",r);
+
+let s = Student{name:"jkjkjk",sex:"girl"};
+
+println!("the student'name is :{},sex:{}",&s.name,&s.sex);
+ 
+
+
 
 }
 
