a98ffd584e4eb95d7e65f563255c81b910be7ca1
grety <snl1016@163.com>
2022-07-30 19:19:55 +0800

    enter

--- smart_point/src/lib.rs
+++ smart_point/src/lib.rs
@@ -1,11 +1,31 @@
 #![allow(unused)]
 #![allow(dead_code)]
+use std::ops::Deref;
+
 
 #[derive(Debug)]
 struct Student{
     name:String,
     phone:String,
 }
+
+struct MyBox<T>(T);
+
+impl<T> Deref for MyBox<T>{
+    type Target = T;
+    fn deref(&self) -> &T {        
+        & self.0
+    }
+}
+
+impl<T> MyBox<T>{
+    fn new(x:T) -> MyBox<T>{
+        MyBox(x)
+    }
+}
+
+
+
 
 pub fn smart_point(){
     {
@@ -17,13 +37,22 @@ pub fn smart_point(){
         // println!("before the s.name is {},s.phone is{}",&x.name,&x.phone);
         // print!("x is {:?}",x);
     }
-    let mut x = 5;
-    let mut x1 = Box::new(x);
-    *x1 = 7;
-    println!("{}",x1);
+    
+    {
+        // let mut x = 5;
+        // let mut x1 = Box::new(x);
+        // *x1 = 7;
+        // println!("{}",x1);
 
-    let mut s = Student{name:String::from("jkjkjk"),phone:String::from("jkjkjk")};
-    let mut s1 = Box::new(s);
-    s1.name = "jkjkjk".to_string();
+        // let mut s = Student{name:String::from("jkjkjk"),phone:String::from("jkjkjk")};
+        // let mut s1 = Box::new(s);
+        // s1.name = "jkjkjk".to_string();
+    }
+    let x = "rust".to_string();
+    let y = MyBox::new(x);
+    hello(&y);
 
+}
+fn hello(x:&str){
+    println!("{}",x);
 }
