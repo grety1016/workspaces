3f3d29f8338c99d7e766fc6e527cf9c777594969
grety <snl1016@163.com>
2022-08-01 11:53:13 +0800

    enter

--- smart_point/src/lib.rs
+++ smart_point/src/lib.rs
@@ -6,30 +6,7 @@
 #![allow(unused)]
 #![allow(dead_code)]
 use std::ops::Deref;
-
-
-#[derive(Debug)]
-struct Student{
-    name:String,
-    phone:String,
-}
-
-struct MyBox<T>(T);
-
-impl<T> Deref for MyBox<T>{
-    type Target = T;
-    fn deref(&self) -> &T {        
-        & self.0
-    }
-}
-
-impl<T> MyBox<T>{
-    fn new(x:T) -> MyBox<T>{
-        MyBox(x)
-    }
-}
-
-
+use std::rc::Rc;
 /// # this is title
 /// ## secend title
 /// &emsp;&emsp;this is all part
@@ -52,8 +29,62 @@ impl<T> MyBox<T>{
 /// 
 /// 
 
+#[derive(Debug)] 
+struct Student{
+    name:String,
+    phone:String,
+}
+
+#[derive(Debug)]
+struct MyBox<T>(T);
+
+impl<T> Deref for MyBox<T>{
+    type Target = T;
+    fn deref(&self) -> &T {        
+        & self.0
+    }
+
+    
+}
+
+impl<T> MyBox<T>{
+    fn new(x:T) -> MyBox<T>{
+        MyBox(x)
+    }
+}
+
+
+
+
+
+#[derive(Debug)]
+enum List{
+    Cons(i32,Box<List>),
+    Nil,
+}
+
+#[derive(Debug)]
+struct Dog{
+    name:String,
+    count:i32,
+}
+
+impl Drop for Dog{
+    fn drop(&mut self){
+        println!("Dog leave");
+    }
+}
+
+#[derive(Debug)]
+enum List2{
+    Cons(i32,Rc<List2>),
+    Nil,
+}
+
+
 
 pub fn smart_point(){
+    //智能指针使用
     {
         // let mut  s = Student{name:"suninglv".to_string(),phone:"15345923407".to_string()};
         // let mut x = Box::new(s);
@@ -63,7 +94,7 @@ pub fn smart_point(){
         // println!("before the s.name is {},s.phone is{}",&x.name,&x.phone);
         // print!("x is {:?}",x);
     }
-    
+    //智能指针实现对类的引用
     {
         // let mut x = 5;
         // let mut x1 = Box::new(x);
@@ -74,13 +105,45 @@ pub fn smart_point(){
         // let mut s1 = Box::new(s);
         // s1.name = "jkjkjk".to_string();
     }
-    
+//智能指针实现dref多态    
     {
         // let x = "rust".to_string();
-        // let t = "rust".to_string();
         // let y = MyBox::new(x);
         // hello(&y);
     }
+//使用智能指针实现链表   
+    {
+        // use List::{Cons,Nil};
+        // let list = Cons(1, Box::new(Cons(2,
+        //                             Box::new(Cons(3,
+        //                                 Box::new(Nil))))));
+
+        // println!("the list is: {:?} ",list);    
+        // let x = MyBox::new(Cons(1, Box::new(Nil))); 
+        // println!("the x is : {:?}",x);      
+
+        
+    }    
+//提前调用drop
+    {
+        // let dog1 = Dog{name:String::from("dahunang"),count:1,};
+        // drop(dog1);
+        // println!("dog1 leave the region.");
+    }
+//使用rc智能指针
+    {
+    // use List2::{Cons,Nil};
+
+    // let a = Rc::new(Cons(5, Rc::new(Nil)));
+    // let b = Cons(6,Rc::clone(&a));
+    // let c = Cons(7,a.clone());
+
+    // println!("a: {:?},b:{:?},c:{:?}",a,b,c);
+
+
+    }
+
+    
 
 
 }
